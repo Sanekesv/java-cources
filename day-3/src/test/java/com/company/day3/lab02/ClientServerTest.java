c53948155ff8620d1b1a831c5529a06e8ff40761
sanekesv <sanekesv@gmail.com>
2015-04-10 20:43:54 +0300

    day 04

--- day-3/src/test/java/com/company/day3/lab02/ClientServerTest.java
+++ day-3/src/test/java/com/company/day3/lab02/ClientServerTest.java
@@ -3,9 +3,11 @@ package com.company.day3.lab02;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class ClientServerTest {
 
     @Test
+    @org.junit.Ignore
     public void test(){
         AnnotationConfigApplicationContext contextClient = new AnnotationConfigApplicationContext(ClientAppConfig.class);
         Greeter obj = contextClient.getBean(Greeter.class);
